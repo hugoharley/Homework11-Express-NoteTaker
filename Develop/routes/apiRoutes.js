4653cae113dcceb14ec5d625336810d3527cff50
Hugo Harley <hpierrel@gmail.com>
2020-03-10 00:37:17 -0400

    better code did the routting

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,43 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+var fs = require('fs');
+var path = require("path");
+let database = require("../db/db.json");
+let noteID = 0;
+
+module.exports = function (app) {
+
+
+    app.get("/api/notes", function (req, res) {
+
+        res.json(database);
+    });
+
+
+
+    app.post("/api/notes", function (req, res) {
+
+        let newNote = req.body;
+        newNote.id = noteID++; //this is the note
+        database.push(newNote);
+        res.json(newNote);
+
+
+    });
+
+
+
+    app.delete("/api/notes/:id", function (req, res) {
+        let noteDelete = req.params.id;
+        for (let i = 0; i < database.length; i++) {
+            let del = database[i];
+            if (del.id == noteDelete) {
+                database.splice(i);
+                res.json(true)
+            }
+        }
+    });
+};
--- Develop/server.js
+++ Develop/server.js
@@ -3,11 +3,14 @@ var express = require("express");
 
 var app = express();
 var PORT = 3000;
+var path = require("path");
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());//you're going to be getting data in post and handling it as json
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 
 require("./routes/apiRoutes")(app);
